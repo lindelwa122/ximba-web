a4241438c245d668e6191e0d4ed90147de5d21d0
Nqabenhle Mlaba <nqabe@LAPTOP-BPKPP5HE.localdomain>
2022-11-28 22:43:56 +0200

    Add confirm email page

--- getvyt/invite/urls.py
+++ getvyt/invite/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'invite'
 
 urlpatterns = [
-    path('', views.register_view, name='register')
+    path('', views.register_view, name='register'),
+    path('confirm', views.confirm_email, name='confirm_email')
 ]
--- getvyt/invite/views.py
+++ getvyt/invite/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def register_view(request):
+    # Your code goes here
+
     return render(request, 'invite/register.html')
+
+def confirm_email(request):
+    # Your code goes here
+
+    return render(request, 'invite/confirm_email.html')
