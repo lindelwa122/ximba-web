00bd38daab5a5a88aedd616d321def9d309e70d5
Nqabenhle <93312182+nqabenhle@users.noreply.github.com>
2022-11-30 22:52:58 +0200

    Fix file structure

--- getvyt/invite/views.py
+++ getvyt/invite/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'invite/index.html')
+    
 def register_view(request):
     # Your code goes here
 
