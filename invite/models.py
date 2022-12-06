b86f36dec63334ff272415a2c28343dc6117704e
Nqabenhle <93312182+nqabenhle@users.noreply.github.com>
2022-12-06 22:47:59 +0200

    Modify user model.

--- invite/models.py
+++ invite/models.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+  email_code = models.IntegerField()
+  is_email_confirmed = models.BooleanField(default=False)
