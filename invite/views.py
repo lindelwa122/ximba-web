3653a407ba2cce03deda7e65b0e9c44e87512248
butterrobot2022 <114500548+butterrobot2022@users.noreply.github.com>
2023-01-30 07:55:21 +0200

    Update views.py

--- invite/views.py
+++ invite/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import difflib
 # Create your views here.
 
+def search_items(names, query):
+    result = []
+    for name in names:
+        if query in name:
+            result.append(name)
+    return result
+
+def find_similar(query, names):
+    result = []
+    for name in names:
+        if difflib.SequenceMatcher(None, query, name).ratio() >= 0.50:
+            result.append(name)
+    return result
 
 def index(request):
     return render(request, 'invite/index.html')
@@ -27,4 +41,21 @@ def reset_password(request):
     return render(request, 'invite/reset_password.html')
 
 def search(request):
+    if request.method == 'GET':
+   
+        query = request.GET['search']
+        result = search_items(names, query)
+
+        if result:
+            for item in result:
+               return HttpResponse(f'Exact match results: {item}')
+        else:
+            print("No exact match found.")
+            similar_result = find_similar(query, names)
+            if similar_result:
+                for item in similar_result:
+                   return HttpResponse(f'Similar results: {item}')
+            else:
+                return HttpResponse("No similar results found.")
+
     return render(request, 'invite/search.html')
