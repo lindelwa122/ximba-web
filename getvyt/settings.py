63c287a8c48c7cebd1bb397e25def0a1a326f6c7
Nqabenhle <93312182+nqabenhle@users.noreply.github.com>
2023-02-07 02:41:41 +0200

    Add tester account

--- getvyt/settings.py
+++ getvyt/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from os import environ, path
 from pathlib import Path
 
+from . import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
