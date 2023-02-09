e3e65e82938096680c2eeed20a0d0d02dfdd7177
Muhammad Ahmad <mahmadmaqbool787@gmail.com>
2023-02-09 22:05:29 +0500

    Add search.scss

--- invite/static/invite/js/search.js
+++ invite/static/invite/js/search.js
@@ -3,3 +3,35 @@ document.querySelectorAll('.search-btn').forEach((el) => {
     // TODO
   });
 });
+
+const searchContent = () => {
+  // Creating Elements
+  const container = document.createElement('div');
+  const searchInput = document.createElement('input');
+  const recents = document.createElement('div');
+  const searched = document.createElement('div');
+  const dimmy1 = document.createElement('div');
+  const dimmy2 = document.createElement('div');
+  const dummy = document.createElement('div');
+  const searchTabs = document.createElement('div');
+  // Appending
+  container.appendChild(searchInput);
+  container.appendChild(recents);
+  container.appendChild(searched);
+  recents.appendChild(dimmy1);
+  recents.appendChild(dimmy2);
+  searched.appendChild(searchTabs);
+  searched.appendChild(dummy);
+  // Adding Classes
+  container.classList = 'edit-profile';
+  searchInput.classList = 'search-input';
+  container.classList = 'search-recents';
+  container.classList = 'search-tabs';
+  container.classList = 'search-users';
+  dimmy1.classList = 'dummy';
+  dimmy2.classList = 'dummy';
+  dummy.classList = 'dummy';
+  console.log(container);
+  return container;
+};
+addToMainModalHistory('Search', searchContent, '10px');
