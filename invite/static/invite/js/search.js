80425a6a84d4ac742843b31d416399dacfb82f44
Muhammad Ahmad <mahmadmaqbool787@gmail.com>
2023-02-11 00:00:32 +0500

    Add template of search feature

--- invite/static/invite/js/search.js
+++ invite/static/invite/js/search.js
@@ -13,25 +13,50 @@ const searchContent = () => {
   const searched = document.createElement('div');
   const dimmy1 = document.createElement('div');
   const dimmy2 = document.createElement('div');
-  const dummy = document.createElement('div');
-  const searchTabs = document.createElement('div');
+  const searchTabs = document.createElement('ul');
+  const accounts = document.createElement('li');
+  const area = document.createElement('li');
+  const events = document.createElement('li');
+  const tags = document.createElement('li');
+  const past = document.createElement('li');
+  const upcoming = document.createElement('li');
+  accounts.innerText = 'Accounts';
+  area.innerText = 'Area';
+  events.innerText = 'Events';
+  tags.innerText = 'Tags';
+  past.innerText = 'Past';
+  upcoming.innerText = 'Upcoming';
+
   // Appending
-  container.appendChild(searchInput);
-  container.appendChild(recents);
-  container.appendChild(searched);
-  recents.appendChild(dimmy1);
-  recents.appendChild(dimmy2);
-  searched.appendChild(searchTabs);
-  searched.appendChild(dummy);
+  container.append(searchInput);
+  container.append(recents);
+  recents.append(dimmy1);
+  recents.append(dimmy2);
+  container.append(searched);
+  container.append(searchTabs);
+  searchTabs.appendChild(accounts);
+  searchTabs.appendChild(area);
+  searchTabs.appendChild(events);
+  searchTabs.appendChild(tags);
+  searchTabs.appendChild(past);
+  searchTabs.appendChild(upcoming);
+  console.log(searchTabs);
   // Adding Classes
-  container.classList = 'edit-profile';
-  searchInput.classList = 'search-input';
-  container.classList = 'search-recents';
-  container.classList = 'search-tabs';
+
   container.classList = 'search-container';
+  searchInput.classList = 'search-input';
+  recents.classList = 'search-recents';
+  recents.innerHTML = `<h2>Recents</h2>`;
+  searchTabs.classList = 'search-tabs';
+  searched.classList = 'searched';
+  searched.innerHTML = `<h2>Searched</h2>`;
+
   dimmy1.classList = 'dummy';
   dimmy2.classList = 'dummy';
-  dummy.classList = 'dummy';
   console.log(container);
+  searchInput.addEventListener('focus', () => {
+    searchInput.style.bottom = '76%';
+    recents.style.height = '0';
+  });
   return container;
 };
